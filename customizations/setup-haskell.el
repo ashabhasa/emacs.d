c844a288968b993a89fa71c6f3fa94fed48fee9c
Arber Shabhasa <ashabhasa@gmail.com>
2018-11-12 22:02:11 +0100

    More modification to the haskell set up

--- customizations/setup-haskell.el
+++ customizations/setup-haskell.el
@@ -2,13 +2,13 @@
 (require 'haskell-mode)
 (require 'hindent)
 (require 'haskell-interactive-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 (add-hook 'haskell-mode-hook #'hindent-mode)
 
-(add-hook 'haskell-mode
+(add-hook 'haskell-mode-hook
           (lambda ()
-            ;; (show-paren-mode)
+            (show-paren-mode)
             (smartparens-mode)
             (yas-minor-mode)
             (git-gutter-mode)
@@ -25,23 +25,6 @@
 ;; )
 
 ;; Functions
-
-(defun shm-repl-tab ()
-  "TAB completion or jumping."
-  (interactive)
-  (unless (shm/jump-to-slot)
-    (call-interactively 'haskell-interactive-mode-tab)))
-
-(defun haskell-interactive-toggle-print-mode ()
-  (interactive)
-  (setq haskell-interactive-mode-eval-mode
-        (intern
-         (ido-completing-read "Eval result mode: "
-                              '("fundamental-mode"
-                                "haskell-mode"
-                                "espresso-mode"
-                                "ghc-core-mode"
-                                "org-mode")))))
 
 (defun haskell-insert-doc ()
   "Insert the documentation syntax."
@@ -70,28 +53,38 @@
   (interactive)
 (haskell-move-nested -1))
 
-(defun haskell-who-calls (&optional prompt)
-  "Grep the codebase to see who uses the symbol at point."
-  (interactive "P")
-  (let ((sym (if prompt
-                 (read-from-minibuffer "Look for: ")
-               (haskell-ident-at-point))))
-    (let ((existing (get-buffer "*who-calls*")))
-      (when existing
-        (kill-buffer existing)))
-    (cond
-     ;; Use grep
-     (nil (let ((buffer
-                 (grep-find (format "cd %s && find . -name '*.hs' -exec grep -inH -e %s {} +"
-                                    (haskell-session-current-dir (haskell-session))
-                                    sym))))
-            (with-current-buffer buffer
-              (rename-buffer "*who-calls*")
-              (switch-to-buffer-other-window buffer))))
-     ;; Use ag
-     (t (ag-files sym
-                  "\\.hs$"
-(haskell-session-current-dir (haskell-session)))))))
 
-(define-key haskell-mode-map (kbd "C-c i") 'hindent/reformat-decl)
+(define-key haskell-mode-map (kbd "C-c i") 'hindent-reformat-decl)
+(define-key haskell-mode-map (kbd "C-c f b") 'hindent-reformat-buffer)
+(define-key haskell-mode-map [f8] 'haskell-navigate-imports)
+(define-key haskell-mode-map (kbd "C-c C-u") 'haskell-insert-undefined)
+(define-key haskell-mode-map (kbd "C-c C-a") 'haskell-insert-doc)
+(define-key haskell-mode-map (kbd "C-<return>") 'haskell-simple-indent-newline-indent)
+(define-key haskell-mode-map (kbd "C-s-<right>") 'haskell-move-right)
+(define-key haskell-mode-map (kbd "C-s-<left>") 'haskell-move-left)
+;;(define-key haskell-mode-map (kbd "<space>") 'haskell-mode-contextual-space)
+
+(custom-set-variables
+ '(haskell-process-type 'cabal-repl)
+ '(haskell-process-args-ghci '())
+ '(haskell-notify-p t)
+ '(haskell-stylish-on-save nil)
+ '(haskell-tags-on-save nil)
+ '(haskell-process-suggest-remove-import-lines t)
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-log t)
+ '(haskell-process-reload-with-fbytecode nil)
+ '(haskell-process-use-presentation-mode t)
+ '(haskell-interactive-mode-include-file-name nil)
+ '(haskell-interactive-mode-eval-pretty nil)
+ '(haskell-process-do-cabal-format-string ":!cd %s && unset GHC_PACKAGE_PATH && %s")
+ '(shm-use-hdevtools t)
+ '(shm-use-presentation-mode t)
+ '(shm-auto-insert-skeletons t)
+ '(shm-auto-insert-bangs t)
+ '(haskell-process-show-debug-tips nil)
+ '(haskell-process-suggest-hoogle-imports nil)
+ '(haskell-process-suggest-haskell-docs-imports t)
+'(hindent-style "gibiansky"))
+
 
